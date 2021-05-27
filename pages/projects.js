6cd92e21f042dc14685450f08dd748c6885c1c5e
Rebel1124 <78895361+Rebel1124@users.noreply.github.com>
2021-05-27 02:04:35 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -7,18 +7,40 @@ export default () => (
     <br />
       <h1 id ="Proj" style={{color: "midnightblue"}}>Projects</h1>
         <ul style={{paddingBottom: "45px"}}>
-        <li style={{color: "darkred"}}><b>Capstone Project II - Shopping Cart</b></li>
-        <a target="_blank" href="https://github.com/Rebel1124/Capstone_II">
-          https://github.com/Rebel1124/Capstone_II</a>
-
-        <br></br>
-        <br></br>
-
-
+          
         <li style={{color: "darkred"}}><b>Desi - Resume</b></li>
         <a target="_blank" href="https://github.com/Rebel1124/Desigan_Reddy_Resume">
           https://github.com/Rebel1124/Desigan_Reddy_Resume</a>
+          
+        <br></br>
+        <br></br>
+          
+        <li style={{color: "darkred"}}><b>Desi - Profile</b></li>
+        <a target="_blank" href="https://github.com/Rebel1124/profileTask">
+          https://github.com/Rebel1124/profileTask</a>
+          
+        <br></br>
+        <br></br>
 
+        <li style={{color: "darkred"}}><b>Capstone Project II - Shopping Cart</b></li>
+        <a target="_blank" href="https://github.com/Rebel1124/Capstone_II">
+          https://github.com/Rebel1124/Capstone_II</a>
+          
+        <br></br>
+        <br></br>
+
+        <li style={{color: "darkred"}}><b>Card Match Game</b></li>
+        <a target="_blank" href="https://github.com/Rebel1124/cardMatch">
+          https://github.com/Rebel1124/cardMatch</a>
+          
+        <br></br>
+        <br></br>
+
+        <li style={{color: "darkred"}}><b>Task Manger</b></li>
+        <a target="_blank" href="https://github.com/Rebel1124/taskManager">
+          https://github.com/Rebel1124/taskManager</a>
+                
+          
       </ul>
   </Layout>
 )
